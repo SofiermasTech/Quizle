340c40ec4a1c2cd6ec879e8b14b08dca29e57122
Sofiermas <Volserma@yandex.ru>
2024-07-06 00:42:23 +0300

    ref: animation for section-intro

--- src/js/main.js
+++ src/js/main.js
@@ -148,7 +148,7 @@ function getItemCard(className) {
             btnLoadMore.style.display = 'none';
          }
       }
-     
+
       console.log(currentCategory.length);
    })
 }
@@ -215,3 +215,44 @@ function resizeHandler() {
 };
 
 window.addEventListener('resize', resizeHandler);
+
+
+// Анимация intro
+const sectionIntro = document.querySelector('.intro');
+const iconBtnBuy = document.querySelector('.btn-buy__icon');
+
+function randomCircle(x, y) {
+   return (x + (Math.random() * (y - x)));
+}
+
+// интервал размеров элемента
+const s = randomCircle(20, 28);
+
+// вариант с кружками и транслейтом
+// for (let i = 0; i < 12; i++) {
+//    sectionIntro.innerHTML += '<div class="circle-intro-decor" style=" width: ' + randomCircle(6, 10) + 'px; height: ' + randomCircle(6, 10) + 'px; animation-duration: ' + randomCircle(10, 30) + 's; top: ' + randomCircle(0, 100) + '%; left: ' + randomCircle(0, 100) + '%;"></div>';
+// }
+
+
+// розовые элм. слева
+for (let i = 0; i < 5; i++) {
+   sectionIntro.innerHTML += '<svg class="intro__decor-1" \
+   style=" animation-duration: ' + randomCircle(5, 15) + 's; \
+   top: ' + randomCircle(10, 100) + '%; left: ' + randomCircle(0, 20) + '%; \
+   width: ' + s + 'px; height: ' + s + 'px;"  viewBox="0 0 17 17" fill="none" \
+   xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" \
+   d="M6.80002 3.57002C6.80002 5.4478 5.27778 6.97003 3.40001 6.97003L1.53 6.97003C0.685004 6.97003 -3.34612e-07 7.65504 -3.71548e-07 8.50003V8.50003C-4.08485e-07 9.34503 0.685004 10.03 1.53 10.03L3.40001 10.03C5.27778 10.03 6.80002 11.5523 6.80002 13.43L6.80002 15.47C6.80002 16.315 7.48502 17 8.33002 17V17C9.17501 17 9.86002 16.315 9.86002 15.47L9.86002 13.515C9.86002 11.5903 11.4203 10.03 13.345 10.03L15.47 10.03C16.315 10.03 17 9.34503 17 8.50003V8.50003C17 7.65504 16.315 6.97003 15.47 6.97003L13.345 6.97003C11.4203 6.97003 9.86002 5.40974 9.86002 3.48502L9.86002 1.53C9.86002 0.685004 9.17501 -3.42041e-07 8.33002 -3.78977e-07V-3.78977e-07C7.48502 -4.15913e-07 6.80002 0.685004 6.80002 1.53L6.80002 3.57002Z"\
+   fill="#fb2dc9" fill-opacity=".8" /></svg>';
+};
+
+
+// фиолетовые элм. справа
+for (let i = 0; i < 6; i++) {
+   sectionIntro.innerHTML += '<svg class="intro__decor-5" \
+   style=" animation-duration: ' + randomCircle(5, 15) + 's; \
+   top: ' + randomCircle(0, 80) + '%; right: ' + randomCircle(0, 30) + '%; \
+   width: ' + s + 'px; height: ' + s + 'px;" viewBox="0 0 22 22" fill="none"  xmlns="http://www.w3.org/2000/svg"> \
+   <path fill="#8264FC" fill-rule="evenodd" \
+   d="M8.8 4.62a4.4 4.4 0 0 1-4.4 4.4H1.98a1.98 1.98 0 0 0 0 3.96H4.4a4.4 4.4 0 0 1 4.4 4.4v2.64a1.98 1.98 0 0 0 3.96 0v-2.53a4.51 4.51 0 0 1 4.51-4.51h2.75a1.98 1.98 0 0 0 0-3.96h-2.75a4.51 4.51 0 0 1-4.51-4.51V1.98a1.98 1.98 0 0 0-3.96 0v2.64Z"\
+   clip-rule="evenodd" /> </svg>';
+};
